3ae14c201322bff2d14d3dde6a9a7317d2963df8
Setho Ehrmann <meister.setho.1@web.de>
2022-11-13 14:54:58 +0100

    init version 6.5

---
+++ 6.5/app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+const CONFIG = [
+    'data_file' => APP_PATH . 'data.json',
+    'DBconnectionString' => 'mysql:dbname=glossary;host=localhost;port=3306',
+    'DB_username' => 'setho',
+    'DB_password' => '!<3Shiro1122',
+    'users' => [
+        'asuka@mail.nerv' => '02'
+    ]
+];
---
+++ 6.5/views/index.view.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="row">
+    <div class="col-lg-12 text-center">
+        <h1 class="mt-5">
+            <?php if (isset($_GET['search']) and $_GET['search'] != '') :?>
+            <?= $view_bag['heading_text']?>
+            <span style="color: #1D9BF0" ><?= $view_bag['heading_value'] ?></span>
+            <?php else :?>
+                <?= $view_bag['heading_text']?>
+            <?php endif ?>
+        </h1>
+    </div>
+    </div>
+    <div class="row">
+        <form class="form-inline" action="" method="GET">
+            <div class="form-group">
+                <input type="text" name="search" id="search_id">
+                <button type="submit" value="search">Search</button>
+            </div>
+        </form>
+    </div>
+    <div class="row">
+        <table class="table table-striped">
+        <?php foreach ($model as $item) : ?>
+            <tr>
+                <td><a href="detail.php?term=<?= $item->term ?>"><?= $item->term ?></a></td>
+                <td><?= $item->definition ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </div>
+</div>
